4ae5ff4aec898426f64059132a81b3c2e2017753
Melanie Van Vlasselaer <melanie.vanvlasselaer@gmail.com>
2019-02-19 17:37:28 +0100

    Update style + index.html

--- javascript/circular_bars.js
+++ javascript/circular_bars.js
@@ -34,7 +34,8 @@
 			self.context.clearRect( 0, 0, self.width, self.height );
 			self.context.lineWidth = 10;
 			self.context.fillStyle = "#000";
-			self.context.strokeStyle = "#d30000";
+			/*self.context.strokeStyle = "#d30000";*/
+            self.context.strokeStyle = "pink";
 			self.context.textAlign = "center";
 			
 			self.context.fillText( self.loaded + "%", self.width * .5, self.height * .5 + 2, self.width );
